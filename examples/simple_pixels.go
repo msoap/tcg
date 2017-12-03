0065ce087265fd758cdf6c9e677f30aa50922352
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-12-03 20:53:49 +0300

    Added primitives: HLine,VLine,Box

--- examples/simple_pixels.go
+++ examples/simple_pixels.go
@@ -14,9 +14,19 @@ func main() {
 
 	tg.PrintStr(3, 4, "Hello world!")
 
-	tg.PutPixel(3, 7, tcg.Black)
-	tg.PutPixel(4, 7, tcg.Black)
-	tg.PutPixel(5, 7, tcg.Black)
+	tg.HLine(3, 6, 12, tcg.Black)
+	tg.VLine(3, 6, 5, tcg.Black)
+
+	tg.PutPixel(5, 8, tcg.Black)
+	tg.PutPixel(7, 8, tcg.Black)
+	tg.PutPixel(9, 8, tcg.Black)
+	tg.PutPixel(11, 8, tcg.Black)
+	tg.PutPixel(13, 8, tcg.Black)
+
+	tg.HLine(3, 10, 12, tcg.Black)
+	tg.VLine(15, 6, 5, tcg.Black)
+
+	tg.Box(3, 20, 20, 5, tcg.Black)
 
 	tg.Show()
 	time.Sleep(5 * time.Second)
---
+++ primitives.go
@@ -0,0 +1,23 @@
+package tcg
+
+// HLine - draw horizontal line
+func (tg *Tcg) HLine(x, y int, length int, color int) {
+	for i := 0; i < length; i++ {
+		tg.PutPixel(x+i, y, color)
+	}
+}
+
+// VLine - draw vertical line
+func (tg *Tcg) VLine(x, y int, length int, color int) {
+	for i := 0; i < length; i++ {
+		tg.PutPixel(x, y+i, color)
+	}
+}
+
+// Box - draw box line
+func (tg *Tcg) Box(x, y int, width, height int, color int) {
+	tg.HLine(x, y, width, color)
+	tg.HLine(x, y+height-1, width, color)
+	tg.VLine(x, y, height, color)
+	tg.VLine(x+width-1, y, height, color)
+}
