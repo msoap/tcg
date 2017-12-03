a5ec9cd94b2e1337270a2abe8d589e7d1d5db407
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-12-03 21:10:59 +0300

    Improved example

--- examples/simple_pixels.go
+++ examples/simple_pixels.go
@@ -15,18 +15,14 @@ func main() {
 	tg.PrintStr(3, 4, "Hello world!")
 
 	tg.HLine(3, 6, 12, tcg.Black)
-	tg.VLine(3, 6, 5, tcg.Black)
 
-	tg.PutPixel(5, 8, tcg.Black)
-	tg.PutPixel(7, 8, tcg.Black)
-	tg.PutPixel(9, 8, tcg.Black)
-	tg.PutPixel(11, 8, tcg.Black)
-	tg.PutPixel(13, 8, tcg.Black)
+	tg.PutPixel(4, 8, tcg.Black)
+	tg.PutPixel(6, 8, tcg.Black)
+	tg.PutPixel(8, 8, tcg.Black)
+	tg.PutPixel(10, 8, tcg.Black)
+	tg.PutPixel(12, 8, tcg.Black)
 
-	tg.HLine(3, 10, 12, tcg.Black)
-	tg.VLine(15, 6, 5, tcg.Black)
-
-	tg.Box(3, 20, 20, 5, tcg.Black)
+	tg.Box(3, 10, 20, 5, tcg.Black)
 
 	tg.Show()
 	time.Sleep(5 * time.Second)
