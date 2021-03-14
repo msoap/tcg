87fc87412ec7ff0f37352a5a036817fcf17d9377
Sergey Mudrik <sergey.mudrik@gmail.com>
2021-03-14 23:52:34 +0200

    Added manual snake example

---
+++ examples/manual_snake/manula_snake.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/msoap/tcg"
+)
+
+func main() {
+	tg, err := tcg.New(tcg.Mode2x3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	maxX, maxY := tg.Width, tg.Height
+	x, y := maxX/2, maxY/2
+	drawNext := func() {
+		tg.PutPixel(x, y, tcg.Black)
+		tg.Show()
+	}
+
+LOOP:
+	for {
+		ev := tg.TCellScreen.PollEvent()
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			switch ev.Key() {
+			case tcell.KeyDown:
+				y++
+				drawNext()
+			case tcell.KeyUp:
+				y--
+				drawNext()
+			case tcell.KeyLeft:
+				x--
+				drawNext()
+			case tcell.KeyRight:
+				x++
+				drawNext()
+			case tcell.KeyEscape:
+				break LOOP
+			}
+		}
+	}
+
+	tg.Finish()
+}
