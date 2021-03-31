e8784ed106d24f3a29b14a803dea4e4c60983ed3
Sergey Mudrik <sergey.mudrik@gmail.com>
2021-03-31 21:14:52 +0300

    Refactoring

--- tcg.go
+++ tcg.go
@@ -3,26 +3,13 @@
 package tcg
 
 import (
-	"errors"
-
 	"github.com/gdamore/tcell/v2"
 )
-
-// PixelsInChar - a type representing the graphics mode
-type PixelsInChar int
 
 // pixel colors
 const (
 	White = 0
 	Black = 1
-)
-
-// graphics modes
-const (
-	Mode1x1 PixelsInChar = iota
-	Mode1x2
-	Mode2x2
-	Mode2x3
 )
 
 var defaultStyle = tcell.StyleDefault.Foreground(tcell.ColorDefault)
@@ -34,66 +21,6 @@ type Tcg struct {
 	Width, Height int // screen width/height in pixels
 	TCellScreen   tcell.Screen
 	Buffer        Buffer
-}
-
-// Width - returns the width in pixels of one character in the text console
-func (pic PixelsInChar) Width() int {
-	switch pic {
-	case Mode1x1, Mode1x2:
-		return 1
-	case Mode2x2, Mode2x3:
-		return 2
-	default:
-		return 0
-	}
-}
-
-// Height - returns the height in pixels of one character in the text console
-func (pic PixelsInChar) Height() int {
-	switch pic {
-	case Mode1x1:
-		return 1
-	case Mode1x2, Mode2x2:
-		return 2
-	case Mode2x3:
-		return 3
-	default:
-		return 0
-	}
-}
-
-// String represent
-func (pic PixelsInChar) String() string {
-	switch pic {
-	case Mode1x1:
-		return "1x1"
-	case Mode1x2:
-		return "1x2"
-	case Mode2x2:
-		return "2x2"
-	case Mode2x3:
-		return "2x3"
-	default:
-		return "-"
-	}
-}
-
-// Set from string (for use with flag.Var())
-func (pic *PixelsInChar) Set(in string) error {
-	switch in {
-	case "1x1":
-		*pic = Mode1x1
-	case "1x2":
-		*pic = Mode1x2
-	case "2x2":
-		*pic = Mode2x2
-	case "2x3":
-		*pic = Mode2x3
-	default:
-		return errors.New("not valid screen mode")
-	}
-
-	return nil
 }
 
 // New - get new object with tcell inside
