a55c60c6f8ea0c237ec566da6dcaaf3acf875c77
Sergey Mudrik <sergey.mudrik@gmail.com>
2021-05-01 23:45:54 +0300

    Added more test cases for Fill

--- drawings_test.go
+++ drawings_test.go
@@ -118,4 +118,34 @@ func TestBuffer_Fill(t *testing.T) {
 		t.Log("\n" + strings.Join(b.Strings(), "\n"))
 		require.True(t, MustNewBufferFromStrings(expected).IsEqual(b))
 	}
+	{
+		b := MustNewBufferFromStrings([]string{
+			"..........",
+			".*******..",
+			".*.....*..",
+			".*...**...",
+			".*....**..",
+			".*......*.",
+			".*.***.*..",
+			".*.*.*.*..",
+			"..*..***..",
+			"..........",
+		})
+		b.Fill(3, 3)
+
+		expected := []string{
+			"..........",
+			".*******..",
+			".*******..",
+			".******...",
+			".*******..",
+			".********.",
+			".*******..",
+			".***.***..",
+			"..*..***..",
+			"..........",
+		}
+		t.Log("\n" + strings.Join(b.Strings(), "\n"))
+		require.True(t, MustNewBufferFromStrings(expected).IsEqual(b))
+	}
 }
