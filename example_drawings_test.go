7e6e59d966ab4c9f9151fbbf709cf884df90a49d
Sergey Mudrik <sergey.mudrik@gmail.com>
2021-06-21 00:49:29 +0300

    Added more drawing examples

--- example_drawings_test.go
+++ example_drawings_test.go
@@ -42,3 +42,93 @@ func ExampleBuffer_VLine() {
 	// .....*....
 	// .....*....
 }
+
+func ExampleBuffer_Rect() {
+	b := tcg.NewBuffer(10, 10)
+	b.Rect(1, 1, 8, 8, tcg.Black)
+	fmt.Println(strings.Join(b.Strings(), "\n"))
+
+	// Output:
+	// ..........
+	// .********.
+	// .*......*.
+	// .*......*.
+	// .*......*.
+	// .*......*.
+	// .*......*.
+	// .*......*.
+	// .********.
+	// ..........
+}
+
+func ExampleBuffer_FillRect() {
+	b := tcg.NewBuffer(10, 10)
+	b.FillRect(1, 1, 8, 8, tcg.Black)
+	fmt.Println(strings.Join(b.Strings(), "\n"))
+
+	// Output:
+	// ..........
+	// .********.
+	// .********.
+	// .********.
+	// .********.
+	// .********.
+	// .********.
+	// .********.
+	// .********.
+	// ..........
+}
+
+func ExampleBuffer_Line() {
+	b := tcg.NewBuffer(10, 10)
+	b.Line(0, 0, 9, 9, tcg.Black)
+	fmt.Println(strings.Join(b.Strings(), "\n"))
+
+	// Output:
+	// *.........
+	// .*........
+	// ..*.......
+	// ...*......
+	// ....*.....
+	// .....*....
+	// ......*...
+	// .......*..
+	// ........*.
+	// .........*
+}
+
+func ExampleBuffer_Circle() {
+	b := tcg.NewBuffer(10, 10)
+	b.Circle(5, 5, 4, tcg.Black)
+	fmt.Println(strings.Join(b.Strings(), "\n"))
+
+	// Output:
+	// ..........
+	// ...*****..
+	// ..**...**.
+	// .**.....**
+	// .*.......*
+	// .*.......*
+	// .*.......*
+	// .**.....**
+	// ..**...**.
+	// ...*****..
+}
+
+func ExampleBuffer_Arc() {
+	b := tcg.NewBuffer(10, 10)
+	b.Arc(5, 5, 4, 45, 225, tcg.Black)
+	fmt.Println(strings.Join(b.Strings(), "\n"))
+
+	// Output:
+	// ..........
+	// ...*****..
+	// ..**...**.
+	// .**.......
+	// .*........
+	// .*........
+	// .*........
+	// .**.......
+	// ..........
+	// ..........
+}
