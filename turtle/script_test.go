0499db92e2d794418d9b0d87730a0e34c9f2ab41
Sergey Mudrik <sergey.mudrik@gmail.com>
2021-07-03 23:30:23 +0300

    Updated test

--- turtle/script_test.go
+++ turtle/script_test.go
@@ -1,10 +1,11 @@
 package turtle
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/msoap/tcg"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTurtle_DrawScript(t *testing.T) {
@@ -28,7 +29,26 @@ func TestTurtle_DrawScript(t *testing.T) {
 			".....*.*..",
 			"......*...",
 		}
-		// t.Log("\n" + strings.Join(b.Strings(), "\n"))
-		require.True(t, tcg.MustNewBufferFromStrings(expected).IsEqual(b))
+		assertEqBuffers(t, b, tcg.MustNewBufferFromStrings(expected))
+	}
+}
+
+func assertEqBuffers(t *testing.T, got, expected tcg.Buffer) {
+	if got.Width != expected.Width {
+		t.Errorf("buffer width of got (%d) != expected (%d)", got.Width, expected.Width)
+		return
+	}
+	if got.Height != expected.Height {
+		t.Errorf("buffer height of got (%d) != expected (%d)", got.Height, expected.Height)
+		return
+	}
+
+	if !expected.IsEqual(got) {
+		gotStrings, expectedStrings := got.Strings(), expected.Strings()
+		msg := fmt.Sprintf("buffers isn't equal:\n%-*s | %-*s\n", got.Width, "got", expected.Width, "expected")
+		for y := 0; y < got.Height; y++ {
+			msg += fmt.Sprintf("%-*s | %-*s\n", got.Width, gotStrings[y], expected.Width, expectedStrings[y])
+		}
+		assert.True(t, false, msg)
 	}
 }
